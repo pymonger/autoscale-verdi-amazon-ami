f6a2e430d71683b478f261904d2ed9f32aad09a0
gmanipon <gmanipon@jpl.nasa.gov>
2017-11-08 09:56:11 -0800

    create work dir and set permissions correctly

--- etc/rc.d/init.d/start-verdi.sh
+++ etc/rc.d/init.d/start-verdi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export HYSDS_HOME=$HOME
 export DATA_DIR=/data
+mkdir -p ${DATA_DIR}/work
+chown -R ops:ops $DATA_DIR
 cd /home/ops/verdi/ops/hysds-dockerfiles/verdi
 /usr/local/bin/docker-compose up -d
