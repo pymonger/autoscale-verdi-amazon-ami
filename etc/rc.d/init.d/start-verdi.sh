cdbbdcd98cfb5d13b1267e5d69912744a6096bc1
gmanipon <gmanipon@jpl.nasa.gov>
2018-02-13 15:26:35 +0000

    set new required env variables: HOST_UID and HOST_GID

--- etc/rc.d/init.d/start-verdi.sh
+++ etc/rc.d/init.d/start-verdi.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 export HYSDS_HOME=$HOME
 export DATA_DIR=/data
+export HOST_UID=$(id -u)
+export HOST_GID=$(id -g)
 mkdir -p ${DATA_DIR}/work
-chown -R ops:ops $DATA_DIR
-cd /home/ops/verdi/ops/hysds-dockerfiles/verdi
+chown -R ${HOST_UID}:${HOST_GID} $DATA_DIR
+cd ${HOME}/verdi/ops/hysds-dockerfiles/verdi
 /usr/local/bin/docker-compose up -d
